12eee107cc07a27dd89aec404c471d374ebf1c13
Kadeska <kadeska23@gmail.com>
2022-12-24 14:34:35 -0800

    Fixed links to redirect to selected page.

--- src/php/views/partials/nav.php
+++ src/php/views/partials/nav.php
@@ -3,10 +3,10 @@
     <nav class="menu-parent-nav">
         <button class="menu-button" onclick="toggleMenu()" onmouseover="toggleMenu()">Menu</button>
         <ul class="menu" id="menu">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Projects</a></li>
-            <li><a href="#">Contact</a></li>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="projects.php">Projects</a></li>
+            <li><a href="contact.php">Contact</a></li>
         </ul>
     </nav>
 </header>
